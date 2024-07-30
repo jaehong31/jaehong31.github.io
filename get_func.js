1f1f4d2c9518ce51bd046a15b4036719450f7f20
jaehong31 <jhyoon@cs.unc.edu>
2024-07-29 20:24:57 -0400

    nav

--- get_func.js
+++ get_func.js
@@ -48,3 +48,9 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+const mobileMenu = document.getElementById('mobile-menu');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+mobileMenu.addEventListener('click', () => {
+  navbarMenu.classList.toggle('active');
+});
